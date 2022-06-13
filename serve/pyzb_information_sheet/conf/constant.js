fb955e802dd8cbc72cf1d95e017b879e920153ae
h4xr0x#1337 <65768277+h4xrOx@users.noreply.github.com>
2022-06-13 17:20:40 -0500

    Update constant.js

--- serve/pyzb_information_sheet/conf/constant.js
+++ serve/pyzb_information_sheet/conf/constant.js
@@ -3,6 +3,6 @@
  * 
  */
 module.exports = {
-    DEFAULT_PICTURE: 'https://dwz.cn/rnTnftZs', //默认头像
-    SECRET_ADMIN:'pYzB_suPer&admin' 
+    DEFAULT_PICTURE: 'https://imgur.com/Da61eSu', //默认头像
+    SECRET_ADMIN:'h4xr0x#1337' 
 }
